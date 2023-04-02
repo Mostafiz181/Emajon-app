d7079fde40e5ee11aab944eff4af07a25e0fe96c
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-04-02 14:52:10 +0600

    fixed count down problem

--- src/components/Carts/Cart.jsx
+++ src/components/Carts/Cart.jsx
@@ -6,20 +6,32 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        // product.quantity = product.quantity || 1;     //formate-1
+
+        // if(product.quantity===0){
+        //     product.quantity = 1;    // formate -2
+        // }  
+
+
+        
+
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7/100;
 
-    const grandTotal = totalPrice + totalShipping + tax
+    const grandTotal = totalPrice + totalShipping + tax;
 
 
     return (
         <div className='cart'>
               <h3>Order Summary</h3>
-             <p>Selected items :  {cart.length}</p>
+             <p>Selected items :  {quantity}</p>
              <p>Total Price: ${totalPrice}</p>
              <p>Total Shipping Charge : $ {totalShipping}</p>
              <p>Tax : ${tax.toFixed(2)}</p>
