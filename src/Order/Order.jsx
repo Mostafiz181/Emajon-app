87b993a1041a16d4d9dbcf3c5e11e95f50c6e2fc
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-04-06 17:37:35 +0600

    added cart in different route and add img

--- src/Order/Order.jsx
+++ src/Order/Order.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Cart from '../components/Carts/Cart';
 import { useLoaderData } from 'react-router-dom';
+import Product from '../components/Product/Product';
+import ReviewItem from '../components/ReviewItem/ReviewItem';
 
 const Order = () => {
     const cart = useLoaderData();
@@ -11,6 +13,14 @@ const Order = () => {
         <div className='shop-container'>
            <div className='product-container'>
                 <h4>this is our order: {cart.length}  </h4>
+                {
+                    cart.map(product=> <ReviewItem
+
+                        key={product.id}
+                        product={product}
+                    
+                    ></ReviewItem>)
+                }
            </div>
 
            <div className='shop-details'>
